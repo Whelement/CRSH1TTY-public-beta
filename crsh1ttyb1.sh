dd10478eaa12555b4d84f4961068f2784d555759
Kelsea <150978109+chrossystem@users.noreply.github.com>
2024-01-28 13:55:35 -0800

    Github is stupid

--- crsh1ttyb1.sh
+++ crsh1ttyb1.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+# CRSH1TTY BUILD 1 / BETA #1
+
+# patch notes:
+# - none, check back next build (few days / hours)
+
+force=true # why did i name it this lmfaooo
+print_codes=true
+instances=1
+
+if grep -q "warning: script from noexec mount" "$0"; then
+    echo "ignore that warning ^"
+fi
+
+echo "|--------------------------|"
+read -p "| Do you want to clear the console each time it tries a code? (y/n): | " answer && { [ "$answer" == "y" ] && fast=1 || fast=2; }
+echo "|--------------------------|"
+read -p "| Do you want to print codes to console? (y/n): | " answer && { [ "$answer" == "y" ] && print_codes=true || print_codes=false; }
+echo "|--------------------------|"
+read -p "| Enter the number of instances to run (minimum 1): | " user_instances
+if [[ "$user_instances" =~ ^[0-9]+$ && "$user_instances" -ge 1 ]]; then
+    instances=$user_instances
+fi
+echo "|--------------------------|"
+
+generate_code() {
+  characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  ac=""
+
+  for ((j=0; j<8; j++)); do
+    ac+=${characters:$((RANDOM%36)):1}
+  done
+}
 
 main() {
   while $force; do 
@@ -6,8 +38,29 @@ main() {
     if [ "$fast" != "2" ]; then
       clear
       echo "CRSH1TTY Public Beta #1 - build 1" # just gonna list this as build 1, all subsequent releases will be built off of this haha
+      echo "
+        @@@@        
+    @@@@@@@@@@@@    
+   @@@@@@@@@@@@@@   
+  @ @@@@            
+ @@@ @  @@@@  @@@@@ 
+ @@@@  @@@@@@ @@@@@ 
+ @@@@@  @@@@  @@@@@ 
+  @@@@@@     @@@@@  
+   @@@@@@@@ @@@@@   
+    @@@@@@ @@@@@    
+        @ @@        
+        "
     fi
-    echo "Trying the code $ac"
+    if [ "$print_codes" == true ]; then
+        echo "Trying the code $ac"
+    else
+        ((count++))
+        if [ "$count" -eq 10000 ]; then
+            count=0
+            echo "Generated 10000 codes, please check progress."
+        fi
+    fi
     sudo gsctool -t -r "$ac"
     if [ $? -eq 0 ]; then
       force=false
@@ -17,13 +70,11 @@ main() {
       sleep 2
       echo "Let's check if write protection is actually off"
       sleep 3
-      crossystem wpsw_cur # more convenient than flashrom, i swear!!!
+      crossystem wpsw_cur # more convenient than flashrom, I swear!!!
       sleep 3
       echo "DM @crossystem about this on Discord and send her the picture."
       sleep 2
       echo "Opening a bash shell for unenrolling..."
-      printf 'echo "Unenrolling..."\nflashrom --wp-disable\nflashrom -p ec --wp-disable\nsudo bash /usr/share/vboot/bin/set_gbb_flags.sh 0x80b0\nfutility gbb --set --flash --flags=0x80b0\ncrossystem block_devmode=0\nvpd -i RW_VPD -d block_devmode -d check_enrollment \ncryptohome --action=remove_firmware_management_parameters\necho "attempting unfog"\ntpm_manager_client take_ownership\nchromeos-tpm-recovery'>unenroll.sh
-      echo "A unenroll.sh file has been dropped, use bash unenroll.sh to unenroll"
       sleep 1
       sudo bash
       break
@@ -31,37 +82,8 @@ main() {
   done
 }
 
-# Check if gcstool command is available
-if command -v gcstool &> /dev/null; then
-    # echo "gcstool is available. Continuing..."
-# CRSH1TTY BUILD 1 / BETA #1
-
-# patch notes:
-# - none, check back next build (few days / hours)
-
-force=true # why did i name it this lmfaooo
-
-if grep -q "warning: script from noexec mount" "$0"; then
-    echo "ignore that warning ^"
-fi
-
-read -p "do you want to clear the console each time it tries a code? (y/n): " answer && { [ "$answer" == "y" ] && fast=1 || fast=2; }
-
-generate_code() {
-  characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  code_length=8
-
-  ac=""
-  for ((j=0; j<code_length; j++)); do
-    ac+=${characters:$((RANDOM%${#characters})):1}
-  done
-}
+for ((i=0; i<$instances; i++)); do
+    main &
+done
 
 wait
-main &
-main & 
-main & # this is good for public release, right?
-else
-    echo "gcstool is not available. Exiting..."
-    exit 1
-fi
