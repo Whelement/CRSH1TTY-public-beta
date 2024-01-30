bc6bb205cf3323aaf6482e102d1e969705cb91e2
Kelsea <150978109+chrossystem@users.noreply.github.com>
2024-01-29 21:06:02 -0800

    crsh1ttyb3.sh

--- crsh1ttyb2.sh
+++ crsh1ttyb2.sh
@@ -1,94 +1,33 @@
 #!/bin/bash
-# CRSH1TTY BUILD 2 / BETA #2
 
-# patch notes:
-# - none, check back next build (few days / hours)
+gencode() {
+    characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    code_length=8
+    ac=$(for ((i=0; i<code_length; i++)); do echo -n "${characters:RANDOM%${#characters}:1}"; done)
+}
 
-force=true # why did i name it this lmfaooo
-print_codes=true
-instances=1
+process() {
+    while true; do
+        gencode
+        sudo gsctool -t -r "$ac" && {
+            echo "Auth code found"
+            sudo bash
+        }
+    done
+}
 
 main() {
-  while $force; do 
-    generate_code
-    if [ "$fast" != "2" ]; then
-      clear
-      echo "CRSH1TTY Public Beta #2 - build 2"
-      echo "
-        @@@@        
-    @@@@@@@@@@@@    
-   @@@@@@@@@@@@@@   
-  @ @@@@            
- @@@ @  @@@@  @@@@@ 
- @@@@  @@@@@@ @@@@@ 
- @@@@@  @@@@  @@@@@ 
-  @@@@@@     @@@@@  
-   @@@@@@@@ @@@@@   
-    @@@@@@ @@@@@    
-        @ @@        
-        "
-    fi
-    if [ "$print_codes" == true ]; then
-        echo "Trying the code $ac"
-    else
-        ((count++))
-        if [ "$count" -eq 10000 ]; then
-            count=0
-            echo "Generated 10000 codes, please check progress."
-        fi
-    fi
-    sudo gsctool -t -r "$ac"
-    if [ $? -eq 0 ]; then
-      force=false
-      echo "FOUND IT! Correct code is $ac"
-      sleep 1
-      read -p "Write down your auth code or take a picture and press enter to continue"
-      sleep 2
-      echo "Let's check if write protection is actually off"
-      sleep 3
-      crossystem wpsw_cur # more convenient than flashrom, I swear!!!
-      sleep 3
-      echo "DM @crossystem about this on Discord and send her the picture."
-      sleep 2
-      echo "Opening a bash shell for unenrolling..."
-      sleep 1
-      sudo bash
-      break
-    fi
-  done
+    local sub=$1
+
+    for ((i=0; i<sub; i++)); do
+        (
+            process
+        ) &
+    done
+    wait
 }
 
-if command -v gcstool &> /dev/null; then
+read -rp "WARNING! You cannot terminate the script if you run 6 or more subprocesses!
+Enter the number of subprocesses to run: " sub
 
-if grep -q "warning: script from noexec mount" "$0"; then
-    echo "ignore that warning ^"
-fi
-
-echo "|--------------------------|"
-read -p "| Do you want to clear the console each time it tries a code? (y/n): | " answer && { [ "$answer" == "y" ] && fast=1 || fast=2; }
-echo "|--------------------------|"
-read -p "| Do you want to print codes to console? (y/n): | " answer && { [ "$answer" == "y" ] && print_codes=true || print_codes=false; }
-echo "|--------------------------|"
-read -p "| Enter the number of instances to run (minimum 1): | " user_instances
-if [[ "$user_instances" =~ ^[0-9]+$ && "$user_instances" -ge 1 ]]; then
-    instances=$user_instances
-fi
-echo "|--------------------------|"
-
-generate_code() {
-  characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  ac=""
-
-  for ((j=0; j<8; j++)); do
-    ac+=${characters:$((RANDOM%36)):1}
-  done
-}
-
-for ((i=0; i<$instances; i++)); do
-    main &
-done
-
-wait
-else
-    echo ""
-fi
+main "$sub"
